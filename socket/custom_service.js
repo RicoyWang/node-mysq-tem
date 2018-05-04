d0ba65384cd9a5c2a83176e8b0bb8d180d2179df
Ricoywang <1129487913@qq.com>
2018-05-04 14:56:03 +0800

    整理 socket 代码结构

--- socket/custom_service.js
+++ socket/custom_service.js
@@ -1,35 +1,60 @@
+    class SocketControl {
+        constructor(socket,io){
+            this.socket = socket
+            this.io = io
+            return this
+        }
+        _init(){
+            this._socketLinsentOn_login()
+            this._socketLinsentOn_disconnect()
+            this._socketLinsentOn_postMsg()
+            this._socketLinsentOn_foo()
+        }
+        _socketLinsentOn_login(){
+            let socket = this.socket
+            let io = this.io
+            socket.on('login', function(nickname) {
+                console.log(nickname,users)
+                if (users.indexOf(nickname) > -1) {
+                    socket.emit('nickExisted');
+                } else {
+                    socket.userIndex = users.length;
+                    socket.nickname = nickname;
+                    users.push(nickname);
+                    socket.emit('loginSuccess');
+                    io.sockets.emit('system', nickname,users.length, 'login'); //向所有连接到服务器的客户端发送当前登陆用户的昵称 
+                };
+            })
+        }
+        _socketLinsentOn_disconnect(){
+            let socket = this.socket
+                //断开连接的事件
+            socket.on('disconnect', function() {
+                //将断开连接的用户从users中删除
+                users.splice(socket.userIndex, 1);
+                //通知除自己以外的所有人
+                socket.broadcast.emit('system', socket.nickname, users.length, 'logout');
+            });
+        }
+        _socketLinsentOn_postMsg(){
+            let socket = this.socket
+            socket.on('postMsg', function(msg) {
+                //将消息发送到除自己外的所有用户
+                socket.broadcast.emit('newMsg', socket.nickname, msg);
+            });
+        }
+        _socketLinsentOn_foo(){
+            let socket = this.socket
+            socket.on('foo',(res)=>{
+                console.log(res)
+            })
+        }
+    }
 function customService(io) {
     return function (socket) {
-            //接收并处理客户端发送的foo事件
-        //昵称设置
-        console.log('*****************')
-        socket.on('foo',(res)=>{
-            console.log(res)
-        })
-        
-        socket.on('login', function(nickname) {
-            console.log(nickname,users)
-            if (users.indexOf(nickname) > -1) {
-                socket.emit('nickExisted');
-            } else {
-                socket.userIndex = users.length;
-                socket.nickname = nickname;
-                users.push(nickname);
-                socket.emit('loginSuccess');
-                io.sockets.emit('system', nickname,users.length, 'login'); //向所有连接到服务器的客户端发送当前登陆用户的昵称 
-            };
-        })
-            //断开连接的事件
-        socket.on('disconnect', function() {
-            //将断开连接的用户从users中删除
-            users.splice(socket.userIndex, 1);
-            //通知除自己以外的所有人
-            socket.broadcast.emit('system', socket.nickname, users.length, 'logout');
-        });
-        socket.on('postMsg', function(msg) {
-            //将消息发送到除自己外的所有用户
-            socket.broadcast.emit('newMsg', socket.nickname, msg);
-        });
+
+        var soket = new SocketControl(socket,io)
+        soket._init()
         
     }
   }
---
+++ socket/users.js
@@ -0,0 +1,2 @@
+ const userArr = []
+ module.exports = userArr
